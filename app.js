7fb392477f3dcfb72bcaac1f6f1b1290f43e8157
Eve Mendelevich <ym9382@gmail.com>
2018-11-09 12:49:02 -0500

    added sequelize essentials

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const morgan = require('morgan')
-const layout = require('./views/layout.js')
+const morgan = require('morgan');
+const layout = require('./views/layout.js');
+const models = require('./models');
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded( { extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
+  res.send(layout('hello world'));
+});
 
-  res.send(layout('hello world'))
-})
+// db.authenticate().then(() => {
+//   console.log('connected to the database');
+// });
 
+const init = async () => {
+  await models.db.sync({ force: true });
 
+  const PORT = 1337;
 
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+};
 
-
-const PORT = 1337;
-
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-})
-
+init();
---
+++ models/index.js
@@ -0,0 +1,49 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+  logging: false,
+});
+
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      notNull: true,
+    },
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  status: {
+    type: Sequelize.ENUM('open', 'closed'),
+  },
+});
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true,
+    },
+  },
+});
+
+module.exports = { db, Page, User };
